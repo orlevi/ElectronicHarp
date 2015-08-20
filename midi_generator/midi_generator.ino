95b4b080a56b50f514dac86c196865073147b5c3
netanelf <netanelf@gmail.com>
2015-08-20 22:40:21 +0300

    added datasheets and updated midi player

--- midi_generator/midi_generator.ino
+++ midi_generator/midi_generator.ino
@@ -1,28 +1,41 @@
 #define midi_reset 12
+#define qout  7
+#define clk  8
+#define load 9
+
+int curNote1 = 0;
+int curNote2 = 0;
+int prevNote1 = 0;
+int prevNote2 = 0;
 
 void setup(){
   Serial.begin(31250);
   pinMode(midi_reset, OUTPUT);
-  pinMode(13, OUTPUT);
+  
+  pinMode(qout, INPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(load, OUTPUT);
+  
+  digitalWrite(load, HIGH);
+  digitalWrite(clk, HIGH);
   digitalWrite(midi_reset, HIGH);
   delay(1000);
-  
+  setProg(1, 46);
 }
 
 void loop(){
-  digitalWrite(13, HIGH);
-   for (int i=50;i <80; i++){
-     setProg(1, 46);  
-     playNote(i, 80);
-     delay(200);
-   }
-   //playNote(20, 0x65);
-   //delay(1500);
-   //digitalWrite(13,LOW);
-   //playNote(30, 0x65);
-   //delay(1500);
+   readData();
+   playNotes();
 }
 
+void    playNotes(){
+  if (prevNote1 == 0 && curNote1 == 1){
+    playNote(67, 80);
+  }
+  if (prevNote2 == 0 && curNote2 == 1){
+    playNote(81, 80);
+}
+}
 void setProg(byte channel, byte instrument){
   Serial.write(0xc0 + channel);
   delay(10);
@@ -30,7 +43,32 @@ void setProg(byte channel, byte instrument){
 }
 
 void playNote(byte note, byte velocity){
+  // note off 
+  Serial.write(0x80 +1);
+   Serial.write(note);
+   Serial.write(velocity);
+
+// note on
    Serial.write(0x90 +1);
    Serial.write(note);
    Serial.write(velocity);
+}
+
+void readData(){
+    //load
+    digitalWrite(load, LOW);
+    //delay(50);
+    digitalWrite(load, HIGH);
+    //delay(50);
+    int c = digitalRead(qout);
+    
+    prevNote1 = curNote1;
+    curNote1 = c;
+    digitalWrite(clk, LOW);
+    //delay(5);
+    digitalWrite(clk, HIGH);
+    //delay(5);
+    int c2 = digitalRead(qout);
+    prevNote2 =  curNote2;
+    curNote2 = c2;
 }
