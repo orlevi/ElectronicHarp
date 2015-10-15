bb77d76b6e0b7c9242dcb1d736b06cf581b09426
netanelf <netanelf@gmail.com>
2015-10-15 18:36:54 +0300

    updated to 3 shift registers, key table to better suit a harp

--- midi_generator/midi_generator.ino
+++ midi_generator/midi_generator.ino
@@ -2,10 +2,12 @@
 #define data_in  2
 #define clk  3
 #define load 4
-#define SENSORS_NUM  8
-
-int curr_notes[SENSORS_NUM] = {0,0,0,0,0,0,0,0};
-int prev_notes[SENSORS_NUM] = {0,0,0,0,0,0,0,0};
+#define SENSORS_NUM  24
+#define CHANNEL 1
+#define INSTRUMENT  19//46
+int notes[28] = {48,50,52,53,55,57,59,60,62,64,65,67,69,71,72,74,76,77,79,81,83,84,86,88,89,91,93,95};
+int curr_notes[SENSORS_NUM] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int prev_notes[SENSORS_NUM] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 void setup(){
   Serial.begin(31250);
@@ -19,7 +21,7 @@ void setup(){
   digitalWrite(clk, HIGH);
   digitalWrite(midi_reset, HIGH);
   delay(1000);
-  setProg(1, 46);
+  setProg(1, INSTRUMENT);
 }
 
 void loop(){
@@ -31,7 +33,7 @@ void loop(){
 void    playNotes(){
       for(int i=0; i<SENSORS_NUM; i++){
           if (prev_notes[i] == 0 && curr_notes[i] == 1){
-                playNote(67+i, 80);
+                playNote(notes[28-i], 80);
           }
       }    
 }
@@ -44,12 +46,13 @@ void setProg(byte channel, byte instrument){
 
 void playNote(byte note, byte velocity){
   // note off 
-  Serial.write(0x80 +1);
+   //Serial.write(0x80 +1);
+   Serial.write(0x80 +CHANNEL);
    Serial.write(note);
    Serial.write(velocity);
 
 // note on
-   Serial.write(0x90 +1);
+   Serial.write(0x90 +CHANNEL);
    Serial.write(note);
    Serial.write(velocity);
 }
